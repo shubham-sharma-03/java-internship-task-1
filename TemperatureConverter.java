fe0a0665b89af11082036e7aa0f77de529164372
Shubham Sharma <sharmashubham05275@gmail.com>
2026-04-17 12:07:24 +0530

    Fix formatting issues in TemperatureConverter.java

--- TemperatureConverter.java
+++ TemperatureConverter.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 
 public class TemperatureConverter {
     public static void main(String[] args) {
+        
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter temperature in Fahrenheit: ");
