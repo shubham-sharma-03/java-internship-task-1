73aabd7bbbea9c3f77f4ae7498fe455e696b6d6b
Shubham Sharma <sharmashubham05275@gmail.com>
2026-06-03 21:17:51 +0530

    Format code for PrimeNumberChecker.java

--- PrimeNumberChecker.java
+++ PrimeNumberChecker.java
@@ -10,7 +10,8 @@
             boolean isPrime = true;
 
             if (num <= 1) isPrime = false;
-            else {
+            else
+            {
                 for (int i = 2; i <= Math.sqrt(num); i++) {
                     if (num % i == 0) {
                         isPrime = false;
